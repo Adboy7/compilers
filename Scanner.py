324fc61200617570d22b4af95f789a98a36789cd
Adboy7 <chiodoadrien@gmail.com>
2020-02-28 12:50:26 -0800

    lex struct+int_lit+type_id+object_id done

--- Scanner.py
+++ Scanner.py
@@ -55,7 +55,11 @@ tokens = [
     'equal',
     'lower',
     'lower_equal',
-    'assign'
+    'assign',
+
+    'integer_literal',
+    'type_identifier',
+    'object_identifier'
 
 ] + list(reserved.values())
 
@@ -63,10 +67,12 @@ tokens = [
 
 t_lowercase_letter = r'([a-z])'
 t_uppercase_letter = r'([A-Z])'
-t_letter= r'(' + t_lowercase_letter + r'|' + t_uppercase_letter + r')'
-t_bin_digit = r'[0-1]'
-t_digit = r'(' + t_bin_digit + r'|[2-9])'
-t_hex_digit=r'('+t_digit+r'|[A-Fa-f])'
+t_letter = t_lowercase_letter + r'|' + t_uppercase_letter 
+t_bin_digit = r'([0-1])'
+t_digit = t_bin_digit + r'|[2-9]'
+t_hex_digit = t_digit+r'|[A-Fa-f]'
+t_type_identifier = t_uppercase_letter+r'('+t_letter+r'|'+t_digit+r'| _ )*'
+
 
 #Operators
 t_lbrace = r'{'
@@ -85,16 +91,37 @@ t_dot = r'\.'
 t_equal = r'\='
 t_lower = r'\<'
 t_lower_equal = r'\<\='
-t_assign = "\<-"
+t_assign = r'\<-'
+
+#Regular expression for @TOKEN
+basetendigit = r'('+ t_digit + r'('+t_digit+r')*)'
+basesixteendigit =r'(('+t_hex_digit+r')('+t_hex_digit+r')*)'
+integer_literal =  r'0x'+ basesixteendigit +r'|'+ basetendigit 
+object_identifier = t_lowercase_letter+r'('+t_letter+r'|'+t_digit+r'| _ )*'
+
 
 #Regular expression function rules tokens 
 #WARNING All tokens defined by functions are added in the same order as they appear in the lexer file.
 #So longer expression first !
 
+@TOKEN(object_identifier)
+def t_object_identifier(t):
+     t.type = reserved.get(t.value,'object_identifier')
+     return t
+
+@TOKEN(integer_literal)
+def t_integer_literal(t):
+    if(t.value[:2]==r'0x'):
+        t.value=int(t.value,0)
+    else:
+        t.value=int(t.value)
+    return t
+
 def t_newline(t):
      r'\t+'
      t.lexer.lineno += len(t.value)
-     
+
+
 
 
 t_ignore  = ' '
@@ -105,7 +132,7 @@ def t_error(t):
 
 lexer = lex.lex()
 
-lexer.input(".=<<=<-")
+lexer.input("0x3f 25 A_6 and a_6 ando a6 int32")
 
 while True:
     tok = lexer.token()
