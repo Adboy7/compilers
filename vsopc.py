12ddb589af164fc6c62808b80599fc6fa280b963
h4z31nut <pseudo.nocebo@gmail.com>
2020-03-01 15:00:27 +0100

    better structure + main + colors :s

---
+++ vsopc.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------
+# vsop.py
+#
+# VSOP Main
+#
+#
+# -----------------------------------------------------------------------------
+
+__author__  = "Adrien and Kevin"
+__version__ = '1.0'
+
+import sys
+from vsop_lexer import VsopLexer
+
+class Style:
+    OK = '\033[92m'
+    WARNING = '\x1b[93m'
+    ERROR = '\033[91m'
+    ENDC = '\x1b[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def main(argv):
+    phase = 0
+    files = []
+
+    for arg in argv:
+        if arg == '-h':
+            print("vsop.py -lex <inputfile>")
+            exit()
+        elif arg in ("-lex", "--lexer"):
+            phase = 1
+        else:
+            files.append(arg)
+
+    if phase != 1:
+        print(Style.WARNING + "The only available for now is the lexer"
+            + Style.ENDC)
+        print("vsop.py -lex <inputfile>")
+        exit(1)
+    if not files:
+        print(Style.WARNING + "You must provide at least one file" + Style.ENDC)
+        print("vsop.py -lex <inputfile>")
+        exit(1)
+
+    for f in files:
+        with open(f, 'r') as file:
+            input = file.read()
+
+        # well... no other option for the moment so go lexer i choose you
+        lexer = VsopLexer()
+        tokens = lexer.tokenize(input)
+
+        for t in tokens:
+            print(t)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
