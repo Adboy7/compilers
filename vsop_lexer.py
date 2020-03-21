edd5c7f953811c5f7d763647366b71c0f1e24882
h4z31nut <pseudo.nocebo@gmail.com>
2020-03-21 16:04:52 +0100

    first test parser

--- vsop_lexer.py
+++ vsop_lexer.py
@@ -2,324 +2,352 @@
 # vsop_lexer.py
 #
 # VSOP Lexer
+# lexer == ply.lex
 #
 # REF:
 #   https://www.dabeaz.com/ply/ply.html#ply_nn5
 #   https://github.com/dabeaz/ply/blob/master/ply/lex.py
 #   https://www.programcreek.com/python/example/101132/ply.lex.lexer
 #
+# VERSION:
+#   v1.0: original
+#   v1.1: fix comment error handling
+#   v2.0: simplify code to be handle by the parser
+#
 # -----------------------------------------------------------------------------
 __author__  = "Adrien and Kevin"
-__version__ = '1.0'
+__version__ = '1.1'
 
 import ply.lex as lex
 from ply.lex import TOKEN
 
 class LexicalError(Exception):
-    def __init__(self, line, column, description):
-        self.line = line
-        self.column = column
-        self.description = description
+  def __init__(self, line, column, description):
+    self.line = line
+    self.column = column
+    self.description = description
 
-class VsopLexer:
-    states = (
-        ('comment','exclusive'),
-        ('string','exclusive'),
-    )
+class VsopLexer():
+  states = (
+    ('comment','exclusive'),
+    ('string','exclusive'),
+  )
 
-    escape_char = {
-       'b'  : '\\x08',
-       't'  : '\\x09',
-       'n'  : '\\x0a',
-       'r'  : '\\x0d',
-       '"'  : '\\x22',
-       '\\' : '\\x5c'
-    }
+  escape_char = {
+    'b'  : '\\x08',
+    't'  : '\\x09',
+    'n'  : '\\x0a',
+    'r'  : '\\x0d',
+    '"'  : '\\x22',
+    '\\' : '\\x5c'
+  }
 
-    keywords = {
-        'and' : 'and',
-        'extends' : 'extends',
-        'isnull' : 'isnull',
-        'then' : 'then',
-        'bool' : 'bool',
-        'false' : 'false',
-        'let' : 'let',
-        'true' : 'true',
-        'class' : 'class',
-        'if' : 'if',
-        'new' : 'new',
-        'unit' : 'unit',
-        'do' : 'do',
-        'in' : 'in',
-        'not' : 'not',
-        'while' : 'while',
-        'else' : 'else',
-        'int32' : 'int32',
-        'string' : 'string'
-    }
+  keywords = {
+    'and' : 'and',
+    'extends' : 'extends',
+    'isnull' : 'isnull',
+    'then' : 'then',
+    'bool' : 'bool',
+    'false' : 'false',
+    'let' : 'let',
+    'true' : 'true',
+    'class' : 'class',
+    'if' : 'if',
+    'new' : 'new',
+    'unit' : 'unit',
+    'do' : 'do',
+    'in' : 'in',
+    'not' : 'not',
+    'while' : 'while',
+    'else' : 'else',
+    'int32' : 'int32',
+    'string' : 'string'
+  }
 
-    operators = {
-        'lbrace',
-        'rbrace',
-        'lpar',
-        'rpar',
-        'colon',
-        'semicolon',
-        'comma',
-        'plus',
-        'minus',
-        'times',
-        'div',
-        'pow',
-        'dot',
-        'equal',
-        'lower',
-        'lower_equal',
-        'assign'
-    }
+  operators = {
+    'lbrace',
+    'rbrace',
+    'lpar',
+    'rpar',
+    'colon',
+    'semicolon',
+    'comma',
+    'plus',
+    'minus',
+    'times',
+    'div',
+    'pow',
+    'dot',
+    'equal',
+    'lower',
+    'lower_equal',
+    'assign'
+  }
 
-    tokens = [
-        'integer_literal',
-        'type_identifier',
-        'object_identifier',
-        'string_literal',
-        'eof'
-    ] + list(keywords.values()) + list(operators)
+  tokens = [
+    'integer_literal',
+    'type_identifier',
+    'object_identifier',
+    'string_literal',
+    'eof'
+  ] + list(keywords.values()) + list(operators)
 
-    # Operator specifications
-    t_lbrace = r'{'
-    t_rbrace = r'}'
-    t_lpar = r'\('
-    t_rpar = r'\)'
-    t_colon = r':'
-    t_semicolon = r';'
-    t_comma = r','
-    t_plus = r'\+'
-    t_minus = r'-'
-    t_times = r'\*'
-    t_div = r'/'
-    t_pow = r'\^'
-    t_dot = r'\.'
-    t_equal = r'\='
-    t_lower = r'\<'
-    t_lower_equal = r'\<\='
-    t_assign = r'\<-'
+  # Operator specifications
+  t_lbrace = r'{'
+  t_rbrace = r'}'
+  t_lpar = r'\('
+  t_rpar = r'\)'
+  t_colon = r':'
+  t_semicolon = r';'
+  t_comma = r','
+  t_plus = r'\+'
+  t_minus = r'-'
+  t_times = r'\*'
+  t_div = r'/'
+  t_pow = r'\^'
+  t_dot = r'\.'
+  t_equal = r'\='
+  t_lower = r'\<'
+  t_lower_equal = r'\<\='
+  t_assign = r'\<-'
 
-    # Regular expressions
-    lowercase   = r'[a-z]'
-    uppercase   = r'[A-Z]'
-    bin_digit   = r'[0-1]'
-    digit       = r'[0-9]'
-    hex_digit   = r'[0-9A-Fa-f]'
-    letter       = r'[a-zA-Z]'
-    alphanum    = r'[a-zA-Z0-9]'
+  # Regular expressions
+  lowercase   = r'[a-z]'
+  uppercase   = r'[A-Z]'
+  bin_digit   = r'[0-1]'
+  digit       = r'[0-9]'
+  hex_digit   = r'[0-9A-Fa-f]'
+  letter       = r'[a-zA-Z]'
+  alphanum    = r'[a-zA-Z0-9]'
 
 
-    def __init__(self):
-        self.last_lf_lexpos = -1
-        self.comment_level = []
-        self.string = ''
-        self.last_lexpos = 0
-        self.last_lineno = 0
-        self.last_column = 0
-        self.eof_reached = False
-        self.lexer = lex.lex(module=self)
+  def __init__(self):
+    # self.tokens = []
+    self.errors = []
+    self.last_lf_lexpos = -1
+    self.comment_level = []
+    self.string = ''
+    self.last_lexpos = 0
+    self.last_lineno = 0
+    self.last_column = 0
+    self.eof_reached = False
+    self.lexer = lex.lex(module=self)
 
-    def tokenize(self, text):
-        tokens = []
-        errors = []
-        self.lexer.input(text)
-        while True:
-            try:
-                tok = self.lexer.token()
-                if not tok: break
-                if not hasattr(tok, 'column'):
-                    tok.column = self.find_column(tok)
-                tokens.append(tok)
-            except LexicalError as le:
-                errors.append(le)
-            if self.eof_reached:
-                break
-        return tokens, errors
+    # def input(self, text):
+    #     self.lexer.input(text)
 
-    def find_column(self, token):
-        return token.lexpos - self.last_lf_lexpos
+    # def token(self):
+    #     while True:
+    #         try:
+    #             t = self.lexer.token()
+    #             break
+    #         except LexicalError as le:
+    #             self.errors.append(le)
+    #     return t
 
-    #Regular expression function rules tokens
-    #WARNING All tokens defined by functions are added in the same order as
-    #they appear in the lexer file.
-    #So longer expression first !
+  def tokenize(self, text):
+    tokens = []
+    self.lexer.input(text)
+    while True:
+      try:
+        tok = self.lexer.token()
+        if not tok: break
+        if not hasattr(tok, 'column'):
+          tok.column = self.find_column(tok)
+        tokens.append(tok)
+      except LexicalError as le:
+        self.errors.append(le)
+      if self.eof_reached:
+        break
+    return tokens, self.errors
 
-    @TOKEN(uppercase + r'(' + letter + r'|' + digit + r'| _ )*')
-    def t_type_identifier(self, t):
-        t.type = 'type_identifier'
-        return t
+  def find_column(self, token):
+    return token.lexpos - self.last_lf_lexpos
 
-    @TOKEN(lowercase + r'(' + letter + r'|' + digit + r'| _ )*')
-    def t_object_identifier(self, t):
-        t.type = self.keywords.get(t.value,'object_identifier')
-        return t
+  #Regular expression function rules tokens
+  #WARNING All tokens defined by functions are added in the same order as
+  #they appear in the lexer file.
+  #So longer expression first !
+
+  @TOKEN(uppercase + r'(' + letter + r'|' + digit + r'| _ )*')
+  def t_type_identifier(self, t):
+    t.type = 'type_identifier'
+    return t
+
+  @TOKEN(lowercase + r'(' + letter + r'|' + digit + r'| _ )*')
+  def t_object_identifier(self, t):
+    t.type = self.keywords.get(t.value,'object_identifier')
+    return t
 
 ### COMMENTS
-    @TOKEN(r'//.*')
-    def t_single_line_comment(self, t):
-        pass
+  @TOKEN(r'//.*')
+  def t_single_line_comment(self, t):
+    pass
 
-    @TOKEN(r'\(\*')
-    def t_open_comment(self, t):
-        self.comment_level = [(t.lineno,  self.find_column(t), t.lexpos)]
-        # self.last_lexpos = t.lexpos
-        # self.last_lineno = t.lineno
-        # self.last_column = self.find_column(t)
-        t.lexer.begin('comment')
+  @TOKEN(r'\(\*')
+  def t_open_comment(self, t):
+    self.comment_level = [(t.lineno,  self.find_column(t), t.lexpos)]
+    # self.last_lexpos = t.lexpos
+    # self.last_lineno = t.lineno
+    # self.last_column = self.find_column(t)
+    t.lexer.begin('comment')
 
-    @TOKEN(r'\(\*')
-    def t_comment_open_nested(self, t):
-        self.comment_level.append((t.lineno,  self.find_column(t), t.lexpos))
+  @TOKEN(r'\(\*')
+  def t_comment_open_nested(self, t):
+    self.comment_level.append((t.lineno,  self.find_column(t), t.lexpos))
 
-    @TOKEN(r'\*\)')
-    def t_comment_close(self, t):
-        if len(self.comment_level) > 1:
-            del self.comment_level[-1]
-        else:
-            t.lexer.begin('INITIAL')
+  @TOKEN(r'\*\)')
+  def t_comment_close(self, t):
+    if len(self.comment_level) > 1:
+      del self.comment_level[-1]
+    else:
+      t.lexer.begin('INITIAL')
 
-    @TOKEN(r'.')
-    def t_comment_any(self, t):
-        pass
+  @TOKEN(r'.')
+  def t_comment_any(self, t):
+    pass
 
-    @TOKEN(r'\*\)')
-    def t_close_comment_error(self, t):
-        raise LexicalError(t.lineno, self.find_column(t),
-            "Invalid integer literal")
+  @TOKEN(r'\*\)')
+  def t_close_comment_error(self, t):
+    raise LexicalError(t.lineno, self.find_column(t),
+      "Invalid integer literal")
 
 
 ### STRINGS
-    @TOKEN(r'"')
-    def t_open_string(self, t):
-        self.string = '"'
-        self.last_lexpos = t.lexpos
-        self.last_lineno = t.lineno
-        self.last_column = self.find_column(t)
-        t.lexer.begin('string')
+  @TOKEN(r'"')
+  def t_open_string(self, t):
+    self.string = '"'
+    self.last_lexpos = t.lexpos
+    self.last_lineno = t.lineno
+    self.last_column = self.find_column(t)
+    t.lexer.begin('string')
 
-    @TOKEN(r'"')
-    def t_string_close(self, t):
-        t.type = 'string_literal'
-        self.string += '"'
-        t.value = self.string
-        t.lexpos = self.last_lexpos
-        t.lineno = self.last_lineno
-        t.column = self.last_column
-        t.lexer.begin('INITIAL')
-        return t
+  @TOKEN(r'"')
+  def t_string_close(self, t):
+    t.type = 'string_literal'
+    self.string += '"'
+    t.value = self.string
+    t.lexpos = self.last_lexpos
+    t.lineno = self.last_lineno
+    t.column = self.last_column
+    t.lexer.begin('INITIAL')
+    return t
 
-    @TOKEN(r'\\[btnr\\"]')
-    def t_string_escape_char(self, t):
-        self.string += self.escape_char[t.value[1]]
+  @TOKEN(r'\\[btnr\\"]')
+  def t_string_escape_char(self, t):
+    self.string += self.escape_char[t.value[1]]
 
-    @TOKEN(r'\\\n\ *')
-    def t_string_break(self, t):
-        self.last_lf_lexpos = t.lexpos + 1
-        t.lexer.lineno += 1
+  @TOKEN(r'\\\n\ *')
+  def t_string_break(self, t):
+    self.last_lf_lexpos = t.lexpos + 1
+    t.lexer.lineno += 1
 
-    @TOKEN(r'\\x' + hex_digit + hex_digit)
-    def t_string_hex(self, t):
-        self.string += t.value
+  @TOKEN(r'\\x' + hex_digit + hex_digit)
+  def t_string_hex(self, t):
+    self.string += t.value
 
-    @TOKEN(r'.')
-    def t_string_any(self, t):
-        if t.value == '\\':
-            raise LexicalError(t.lineno, self.find_column(t),
-                "Invalid character in string literal")
-        elif t.value == '\0':
-            raise LexicalError(t.lineno, self.find_column(t),
-                "Invalid character in string literal")
-        elif ord(t.value) < 32 or ord(t.value) > 126:
-            self.string += "\\x" + t.value.encode("hex")
-        else:
-            self.string += t.value
+  @TOKEN(r'.')
+  def t_string_any(self, t):
+    if t.value == '\\':
+      raise LexicalError(t.lineno, self.find_column(t),
+        "Invalid character in string literal")
+    elif t.value == '\0':
+      raise LexicalError(t.lineno, self.find_column(t),
+        "Invalid character in string literal")
+    elif ord(t.value) < 32 or ord(t.value) > 126:
+      self.string += "\\x" + t.value.encode("hex")
+    else:
+      self.string += t.value
 
 
 ### INTEGER
-    @TOKEN(r'0x' + alphanum + r'*')
-    def t_hex_integer_literal(self, t):
-        t.type = 'integer_literal'
-        try:
-            t.value = int(t.value[2:], 16)
-        except ValueError:
-            raise LexicalError(t.lineno, self.find_column(t),
-                "Invalid integer literal")
-        return t
+  @TOKEN(r'0x' + alphanum + r'*')
+  def t_hex_integer_literal(self, t):
+    t.type = 'integer_literal'
+    try:
+      t.value = int(t.value[2:], 16)
+    except ValueError:
+      raise LexicalError(t.lineno, self.find_column(t),
+        "Invalid integer literal")
+    return t
 
-    # @TOKEN(r'0b' + alphanum + r'*')
-    # def t_bin_integer_literal(self, t):
-    #     t.type = 'integer_literal'
-    #     try:
-    #         t.value = int(t.value[2:], 2)
-    #     except ValueError:
-    #         raise LexicalError(t.lineno, self.find_column(t),
-    #             "Invalid integer literal")
-    #     return t
+# @TOKEN(r'0b' + alphanum + r'*')
+# def t_bin_integer_literal(self, t):
+#     t.type = 'integer_literal'
+#     try:
+#         t.value = int(t.value[2:], 2)
+#     except ValueError:
+#         raise LexicalError(t.lineno, self.find_column(t),
+#             "Invalid integer literal")
+#     return t
 
-    @TOKEN(alphanum + r'+')
-    def t_integer_literal(self, t):
-        t.type = 'integer_literal'
-        try:
-            t.value = int(t.value)
-        except ValueError:
-            raise LexicalError(t.lineno, self.find_column(t),
-                "Invalid integer literal")
-        return t
+  @TOKEN(alphanum + r'+')
+  def t_integer_literal(self, t):
+    t.type = 'integer_literal'
+    try:
+      t.value = int(t.value)
+    except ValueError:
+      raise LexicalError(t.lineno, self.find_column(t),
+          "Invalid integer literal")
+    return t
 
 
 ### LINE FEED
-    def t_newline(self, t):
-        r'\n'
-        self.last_lf_lexpos = t.lexpos
-        t.lexer.lineno += 1
+  def t_newline(self, t):
+    r'\n'
+    self.last_lf_lexpos = t.lexpos
+    t.lexer.lineno += 1
 
-    def t_string_newline(self, t):
-        r'\n'
-        line = t.lexer.lineno
-        column = self.find_column(t)
-        self.last_lf_lexpos = t.lexpos
-        t.lexer.lineno += 1
-        raise LexicalError(line, column,
-            "Invalid raw line feed in string literal")
+  def t_string_newline(self, t):
+    r'\n'
+    line = t.lexer.lineno
+    column = self.find_column(t)
+    self.last_lf_lexpos = t.lexpos
+    t.lexer.lineno += 1
+    raise LexicalError(line, column,
+      "Invalid raw line feed in string literal")
 
-    def t_comment_newline(self, t):
-        r'\n'
-        self.last_lf_lexpos = t.lexpos
-        t.lexer.lineno += 1
+  def t_comment_newline(self, t):
+    r'\n'
+    self.last_lf_lexpos = t.lexpos
+    t.lexer.lineno += 1
 
 ### EOF
-    def t_eof(self, t):
-        return None
+  def t_eof(self, t):
+    return None
 
-    def t_string_eof(self, t):
-        self.eof_reached = True
-        raise LexicalError(self.last_lineno, self.last_column,
-            "EOF reached in string literal")
+  def t_string_eof(self, t):
+    self.eof_reached = True
+    raise LexicalError(self.last_lineno, self.last_column,
+      "EOF reached in string literal")
 
-    def t_comment_eof(self, t):
-        self.eof_reached = True
-        raise LexicalError(self.comment_level[-1][0], self.comment_level[-1][1],
-            "EOF reached in comment")
+  def t_comment_eof(self, t):
+    self.eof_reached = True
+    raise LexicalError(self.comment_level[-1][0], self.comment_level[-1][1],
+      "EOF reached in comment")
 
 ### IGNORED CHARS
-    t_ignore  = ' \t\r\f'
-    t_comment_ignore = ''
-    t_string_ignore = ''
+  t_ignore  = ' \t\r\f'
+  t_comment_ignore = ''
+  t_string_ignore = ''
 
 
 ### ERROR RULES
-    def t_error(self, t):
-        t.lexer.skip(1)
-        raise LexicalError(t.lineno, self.find_column(t), "Invalid character")
+  def t_error(self, t):
+    t.lexer.skip(1)
+    raise LexicalError(t.lineno, self.find_column(t), "Invalid character")
 
-    def t_comment_error(self, t):
-        return self.t_error(t)
+  def t_comment_error(self, t):
+    return self.t_error(t)
 
-    def t_string_error(self, t):
-        return self.t_error(t)
+  def t_string_error(self, t):
+    return self.t_error(t)
+
+### MAIN  
+if __name__ == "__main__":
+  import sys
+  text = open(sys.argv[1], 'r').read()
+  lexer = VsopLexer()
+  toks, errors = lexer.tokenize(text)
+  print(errors)
+  print(toks)
--- vsop_parser.py
+++ vsop_parser.py
@@ -2,16 +2,21 @@
 # vsop_parser.py
 #
 # VSOP parser
-#
-# parser == yacc
+# parser == ply.yacc
 #
 # REF:
+#   https://github.com/dabeaz/ply/blob/master/ply/yacc.py
+#   https://www.dabeaz.com/ply/ply.html#ply_nn24
+#   http://www.matthieuamiguet.ch/pages/compilateurs
+#   https://stackoverflow.com/questions/38262552/how-to-encapsulate-the-lex-and-yacc-of-ply-in-two-seperate-class
 #
 # -----------------------------------------------------------------------------
 __author__  = "Adrien and Kevin"
 __version__ = '1.0'
 
 import ply.lex as lex
+import ply.yacc as yacc
+from vsop_lexer import *
 
 class ParseError(Exception):
   def __init__(self, line, column, message):
@@ -22,9 +27,36 @@ class ParseError(Exception):
 
 class VsopParser:
   def __init__(self):
+    self.tokens = VsopLexer.tokens
+    self.lexer = VsopLexer()
+    self.parser = yacc.yacc(module=self)
+    
+
+  def parse(self, text):
+    result = self.parser.parse(text)
+    return result
+
+### GRAMMAR RULES
+  def p_expression(self, p):
+    '''expression : int32
+                  | string
+                  | type_identifier
+                  | object_identifier'''
+    print("hello")
+
+  def p_empty(self, p):
+    'empty :'
     pass
 
-  def parse(self):
+### ERROR HANDLING
+  def p_error(self, p):
     pass
 
-  
+
+### MAIN
+if __name__ == "__main__":
+  import sys
+  text = open(sys.argv[1], 'r').read()
+  parser = VsopParser()
+  result = parser.parse(text)
+  print(result)
